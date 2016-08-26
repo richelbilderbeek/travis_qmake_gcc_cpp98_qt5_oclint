38511a99c306f9ed71bf3cad499ac04bab706a86
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-26 12:31:13 +0200

    Fail if OCLint cannot find a file

--- do_oclint.sh
+++ do_oclint.sh
@@ -16,6 +16,20 @@ h_files=`ls *.h | egrep -v "^ui_.*\.h$"`
   -I/usr/include/qt5/QtGui \
   -I/usr/include/qt5/QtWidgets
 
+cat oclint.log
+
+# Will be 0 if success
+# Will be 1 if fail
+fail=`egrep "Compiler Errors" oclint.log | wc -l`
+
+if [ $fail -eq 1 ]; 
+then
+  echo "OCLint: Compiler error"
+  exit 1
+else
+  echo "OCLint: OK"
+fi
+
 # Will be 1 if success
 # Will be 0 if fail
 success=`egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" oclint.log | wc -l`
@@ -23,9 +37,7 @@ success=`egrep "FilesWithViolations=0 P1=0 P2=0 P3=0" oclint.log | wc -l`
 if [ $success -eq 1 ]; 
 then
   echo "OCLint: OK"
-  cat oclint.log
 else
   echo "OCLint: Fail"
-  cat oclint.log
   exit 1
 fi
