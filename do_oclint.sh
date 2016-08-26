a561ad2f291dcd502e90bb0bb9a421b4e5408150
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-26 11:46:10 +0200

    Give me an OCLint error

--- do_oclint.sh
+++ do_oclint.sh
@@ -3,27 +3,20 @@
 cpp_files=`ls *.cpp | egrep -v "^qrc_.*\.cpp$" | egrep -v "^moc_.*\.cpp$"`
 h_files=`ls *.h | egrep -v "^ui_.*\.h$"`
 
+echo $cpp_files
+echo $h_files
+
 ./oclint-0.10.3/bin/oclint -o oclint.log -max-priority-1 0 -max-priority-2 0 -max-priority-3 0 \
   -disable-rule ShortVariableName \
-  -disable-rule FileNotFound \
   $cpp_files \
   $h_files \
   -- \
   -c -std=c++11 \
-  -I../RibiClasses/CppAbout \
-  -I../RibiClasses/CppHelp \
-  -I../RibiClasses/CppTestTimer \
-  -I../RibiClasses/CppFileIo \
-  -I../RibiClasses/CppMenuDialog \
-  -I../RibiClasses/CppQtAboutDialog \
-  -I../RibiClasses/CppQtHideAndShowDialog \
-  -I../RibiClasses/CppTrace \
   -I/usr/include/c++/5 \
   -I/usr/include/qt5 \
   -I/usr/include/qt5/QtCore \
   -I/usr/include/qt5/QtGui \
-  -I/usr/include/qt5/QtWidgets \
-  > /dev/null
+  -I/usr/include/qt5/QtWidgets
 
 # Will be 1 if success
 # Will be 0 if fail
